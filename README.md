83be75ac34a263d887e5a485a1089d826b55ad18
Pooja Patel <poojaclass1994@gmail.com>
2025-08-25 19:06:54 +0530

    update

--- README.md
+++ README.md
@@ -6,14 +6,20 @@
 
 ![Swap](<Screenshot 2025-08-25 122009.png>)
 
-### Bill
+### Marksheet
 
-![Bill](<Screenshot 2025-08-25 122047.png>)
+![marksheet](<Screenshot 2025-08-25 190325.png>)
 
 ### Interest
 
 ![Interest](<Screenshot 2025-08-25 122117.png>)
 
+### Bill
+
+![Bill](<Screenshot 2025-08-25 122047.png>)
+![Bill-Html](<Screenshot 2025-08-25 190430.png>)
+
 ### Emplyee Salary
 
 ![Emp-Salary](<Screenshot 2025-08-25 122221.png>)
+![Emp-Salary-Html](<Screenshot 2025-08-25 190458.png>)
---
+++ marksheet.js
@@ -0,0 +1,33 @@
+// Marksheet
+
+
+let sname = 'Pooja Patel'
+let maths = 85, english = 78, science = 87;
+let total = maths + english + science;
+let average = total / 3;
+let grade;
+
+if(average>=80)
+{ 
+    grade = 'A';
+}else if(average>=70 && average <=80)
+{
+    grade = 'B';
+}else if(average>=60 && average <=70)
+{
+    grade = 'C';
+}else if(average>=50 && average <=60)
+{
+    grade = 'D';
+}
+else
+{
+    grade = 'F'
+}
+
+console.log('Marksheet');
+console.log('-----------');
+console.log(`Student name : ${sname}`);
+console.log(`Total Marks is: ${total}`);
+console.log(`Percentage is: ${average} %`);
+console.log(`Grade is: ${grade}`);
